b7fdb204bddde9643384a0af2065a6a6544e855e
danipan91 <kelli.pando@aol.com>
2019-05-20 15:47:43 -0700

    Final Template Submission

---
+++ final-template/includes/header.php
@@ -0,0 +1,24 @@
+
+    <header>
+        
+    <span id="register">
+        <button id="registerButton">Register</button>
+    </span>
+        
+    <button class="hamburger">&#9776;</button>
+    <button class="cross">&#735;</button>
+        
+    </header>
+
+    <div class="menu">
+      <ul>
+        <a href="#"><li>About</li></a>
+        <a href="#"><li>Registration</li></a>
+        <a href="#"><li>Course Details</li></a>
+        <a href="#"><li>Event</li></a>
+        <a href="#"><li>FAQS</li></a>
+        <a href="#"><li>Contact</li></a>
+      </ul>
+    </div>
+
+    <h1 class="col span_12_of_12">Ace in the Hole Multisport Events</h1>
---
+++ final-template/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ace in the Hole Multisport Events</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Kelli Pando">
+    <meta name="description" content="Register for Ace in the Hole Multisport Events">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Ace in the Hole Multisport Events, Portland, Oregon">
+    <link rel="stylesheet" type="text/css" href="styles/reset.css">
+ 
+    <link href="https://fonts.googleapis.com/css?family=Play:700|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles/grid.css">
+    
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    
+    <script src="js/jquery-3.4.1.js"></script>
+    <script type="text/javascript" src="js/hamburger.js"></script>
+
+</head>
+
+<body>
+    <?php    
+        include 'includes/header.php'
+    ?>
+            
+    <section>
+        <!-- hero image/slideshow for tablet/dekstop versions only -->    
+        
+        <img id="slideshow" src="images/tablet_desktop.jpg" alt="image placeholder for tablet/desktop slideshow">    
+    </section>
+    
+    <main class="section group">
+        
+        <section id="aboutAce" class=".span_12_of_12">
+            <!-- About Ace in the Hole Multisport Events, including an image -->
+            <h2>About Ace in the Hole Multisport Events</h2>
+
+        <!-- single image contained inside about section, on mobile versions only -->
+            <div id="aboutImage">
+                    <img src="images/mobile.jpg" alt="image placeholder for mobile resolutions">
+            </div>
+
+            <p>
+                Sample text.
+            </p>
+            
+        </section>
+        
+        <section id="registration" class=".span_12_of_12">
+            <!-- Registration Info and Form -->
+            <h2>Registration</h2>
+            <p>
+                Sample text.
+            </p>
+        </section>
+        
+        <section id="courseDetails" class=".span_12_of_12">
+            <!-- Course Details -->
+            <h2>Course Details</h2>
+            
+            <p>
+                Sample text.
+            </p>
+        </section>
+        
+        <section id="aboutEvent" class=".span_12_of_12">
+            <!-- About the Event -->
+            <h2>About the Event</h2>
+            <p>
+                Sample text.
+            </p>
+        </section>
+        
+        <section id="faqs" class=".span_12_of_12">
+            <!-- FAQS -->
+            <h2>FAQS</h2>
+            <p>
+                Sample text.
+            </p>
+            
+        </section>
+        
+        <section id="contact" class=".span_12_of_12">
+            <!-- Contact Form -->
+            <h2>Contact Form</h2>
+            <p>
+                Sample text.
+            </p>
+        </section>
+    </main>
+ 
+    <!-- Footer section -->
+    <?php
+            include 'includes/footer.php'
+    ?>
+    
+</body>
+
+</html>
