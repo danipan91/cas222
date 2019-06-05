74189012ff9d7b8e9ae48514f8ed5956e8c8d0c1
danipan91 <kelli.pando@aol.com>
2019-06-05 15:03:55 -0700

    soft-launch 4 files

---
+++ soft-launch_4/includes/sections/contact.php
@@ -0,0 +1,24 @@
+<section id="contact" class="form" class=".span_12_of_12">
+    <h2 class="formHeader">Contact Us</h2>
+    
+    <form id="contactForm" action="includes/sections/contact_form_process.php" method="POST">
+        <p>Full Name:<br>
+                <input type="text" name="fullName" placeholder="John Smith / Jane Smith">
+        </p>
+            
+        <p>Email:<br>
+            <input type="text" name="emailAddress" placeholder="johnsmith@somewhere.com">
+        </p>
+            
+        <p>Are you an athlete, volunteer, or interested party?<br>
+            <input type="radio" name="partyType" value="athlete">Athlete<br>
+            <input type="radio" name="partyType" value="volunteer">Volunteer<br>
+            <input type="radio" name="partyType" value="interestedParty">Interested Party
+        </p>
+        
+        <p>Please type your question or comment here:<br>
+            <textarea name="message" placeholder="Type message here..." rows="5" cols="40"></textarea>"<br>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
+</section>
---
+++ soft-launch_4/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ace in the Hole Multisport Events</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Kelli Pando">
+    <meta name="description" content="Register for Ace in the Hole Multisport Events">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Ace in the Hole Multisport Events, Portland, Oregon">
+    <link rel="stylesheet" type="text/css" href="styles/reset.css">
+ 
+    <link href="https://fonts.googleapis.com/css?family=Play:700|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles/grid.css">
+    
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    
+    <script src="js/jquery-3.4.1.js"></script>
+    <script type="text/javascript" src="js/hamburger.js"></script>
+    <script type="text/javascript" src="js/navigation_scroll.js"></script>
+    
+</head>
+
+<body>
+    <?php    
+        include 'includes/header.php';
+    
+        include 'includes/slideshow.php';
+    ?>
+
+    <div id="wrapper">
+            <main class="section group">
+        
+            <?php
+                include 'includes/sections/about_ace.php';
+
+                include 'includes/sections/registration.php';
+
+                include 'includes/sections/course_details.php';
+
+                include 'includes/sections/contact.php';
+
+                include 'includes/sections/about_event.php';
+
+                include 'includes/sections/social_media.php';
+
+                include 'includes/sections/weather_feed.php';
+
+                include 'includes/sections/faqs.php';
+            ?>
+        </main>
+    </div>
+ 
+    <!-- Footer section -->
+    <?php
+            include 'includes/footer.php'
+    ?>
+    
+</body>
+
+</html>
